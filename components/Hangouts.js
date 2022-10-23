8ef20e542d9fb9af397c8d8317d278c4215165bd
Savio Andrade <andradesavio9073@gmail.com>
2022-10-23 12:07:45 +0200

    added hangouts

---
+++ components/Hangouts.js
@@ -0,0 +1,28 @@
+import Accordion from "./Accordian";
+import QuestContainer from "./QuestContainer";
+import { Box } from "@mui/material";
+import quests from "../quests";
+import QuestSection from "./QuestSection";
+const StoryQuests = () => {
+  return (
+    <Accordion title={"Hangouts"} icon={"/HangoutIcon.webp"}>
+      <Box className="p-8 w-full grid lg:grid-cols-2 xl:grid-cols-3 justify-center gap-4">
+        {quests.hangouts.map((quest) =>
+          quest.section ? (
+            <QuestSection key={quest.section} title={quest.section} />
+          ) : (
+            <QuestContainer
+              key={quest.title}
+              title={quest.title}
+              subTitle={quest.subtitle}
+              primos={quest.primos}
+              icon={quest.icon}
+            />
+          )
+        )}
+      </Box>
+    </Accordion>
+  );
+};
+
+export default StoryQuests;
--- components/Quests.js
+++ components/Quests.js
@@ -2,11 +2,13 @@ import React from "react";
 import ArchonQuests from "./ArchonQuests";
 import Accordion from "./Accordian";
 import StoryQuests from "./StoryQuests";
+import Hangouts from "./Hangouts";
 const Quests = () => {
   return (
     <Accordion title={"Quests"} icon={"/Quests.webp"}>
       <ArchonQuests />
       <StoryQuests />
+      <Hangouts />
     </Accordion>
   );
 };
