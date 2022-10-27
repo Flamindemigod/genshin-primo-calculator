b0d03b57cec0274273d2f3c1b71d26f36cc4fbe7
Savio Andrade <andradesavio9073@gmail.com>
2022-10-27 11:17:08 +0200

    fixed build issues

---
+++ components/EventContainer.js
@@ -0,0 +1,9 @@
+import { Paper } from "@mui/material";
+import React from "react";
+
+const EventContainer = ({ name, bgImage, hoyolabLink, startDate, endDate }) => {
+  return;
+  <Paper elevation={3}></Paper>;
+};
+
+export default EventContainer;
