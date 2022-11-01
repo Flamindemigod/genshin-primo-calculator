8322675fedd566faef635d130262aaf5ec5c493a
Savio Andrade <andradesavio9073@gmail.com>
2022-11-01 15:46:58 +0100

    added new quests

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,18 @@
 import { Favorite, GitHub } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import Image from "next/future/image";
 
 const Footer = () => {
   return (
     <div className="w-full bg-black bg-opacity-50 text-offWhite-100 flex p-8 flex-col sm:flex-row justify-between">
       <div>
         <Button href={"https://ko-fi.com/flamindemigod"} variant="contained">
-          <img
+          <Image
             width={32}
             height={32}
             src="https://storage.ko-fi.com/cdn/brandasset/kofi_p_logo_nolabel.png"
             alt="Ko-fi Logo"
-          />{" "}
+          />
           Support me on Kofi
         </Button>
       </div>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["storage.ko-fi.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
