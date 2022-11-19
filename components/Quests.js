bb5315be9781a9230865d2b5dd0b7730c045cbea
Savio Andrade <andradesavio9073@gmail.com>
2022-11-19 15:19:48 +0100

    added dynamic imports for quests section

--- components/Quests.js
+++ components/Quests.js
@@ -1,18 +1,42 @@
 import React from "react";
-import ArchonQuests from "./ArchonQuests";
 import Accordion from "./Accordian";
-import StoryQuests from "./StoryQuests";
-import Hangouts from "./Hangouts";
-import WorldQuestSeries from "./WorldQuestsSeries";
-import WorldQuest from "./WorldQuest";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
 const Quests = ({ setPrimos }) => {
+  const ArchonQuests = dynamic(() => import("./ArchonQuests"), {
+    suspense: true,
+  });
+  const StoryQuests = dynamic(() => import("./StoryQuests"), {
+    suspense: true,
+  });
+  const Hangouts = dynamic(() => import("./Hangouts"), {
+    suspense: true,
+  });
+  const WorldQuestSeries = dynamic(() => import("./WorldQuestsSeries"), {
+    suspense: true,
+  });
+  const WorldQuest = dynamic(() => import("./WorldQuest"), {
+    suspense: true,
+  });
+
   return (
     <Accordion title={"Quests"} icon={"/Quests.webp"}>
-      <ArchonQuests setPrimos={setPrimos} />
-      <StoryQuests setPrimos={setPrimos} />
-      <Hangouts setPrimos={setPrimos} />
-      <WorldQuestSeries setPrimos={setPrimos} />
-      <WorldQuest setPrimos={setPrimos} />
+      <Suspense fallback="Loading....">
+        <ArchonQuests setPrimos={setPrimos} />
+      </Suspense>
+      <Suspense fallback="Loading....">
+        <StoryQuests setPrimos={setPrimos} />
+      </Suspense>
+      <Suspense fallback="Loading....">
+        <Hangouts setPrimos={setPrimos} />
+      </Suspense>
+      <Suspense fallback="Loading....">
+        <WorldQuestSeries setPrimos={setPrimos} />
+      </Suspense>
+      <Suspense fallback="Loading....">
+        <WorldQuest setPrimos={setPrimos} />
+      </Suspense>
     </Accordion>
   );
 };
