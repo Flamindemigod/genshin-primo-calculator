b6de39fe5db310586e6351d857e546c126a3a0a6
Savio Andrade <andradesavio9073@gmail.com>
2022-11-26 16:30:44 +0100

    updated meta even more

--- components/Meta.js
+++ components/Meta.js
@@ -3,7 +3,7 @@ import { SERVER } from "../config";
 const Meta = ({
   title = "Genshin Primo Planner",
   url = "https://primos.flamindemigod.com",
-  image = `${SERVER}/Item_Primogem.webp`,
+  image = `${SERVER}/BG.webp`,
   description = "A Genshin Impact Primogem Calculator made by Delta45321 and Flamindemigod. It accounts for almost all sources of primogems, other than Redeemable Codes, Achievements, Web Events and Chests.",
 }) => {
   return (
@@ -19,8 +19,18 @@ const Meta = ({
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={image} />
+      <meta property="og:image:type" content="image/webp" />
+      <meta property="og:image:width" content="3016" />
+      <meta property="og:image:height" content="2000" />
       <meta property="og:description" content={description} />
-      <meta name="twitter:card" content="summary" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:locale" content="en_GB" />
+      <meta property="og:locale:alternate" content="fr_FR" />
+      <meta property="og:locale:alternate" content="zh_CN" />
+      <meta property="og:locale:alternate" content="zh_Hans" />
+      <meta property="og:locale:alternate" content="zh_Hans_CN" />
+
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
